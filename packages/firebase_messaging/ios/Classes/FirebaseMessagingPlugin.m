8beb9b979634800b1ffdea6d0be681ca8b91237f
Varun <varun@gohighlevel.com>
2019-12-10 18:29:25 +0300

    Do not remove badge count

--- packages/firebase_messaging/ios/Classes/FirebaseMessagingPlugin.m
+++ packages/firebase_messaging/ios/Classes/FirebaseMessagingPlugin.m
@@ -220,8 +220,8 @@ static NSObject<FlutterPluginRegistrar> *_registrar;
   // therefore the next line is setting it to 1 first before clearing it again
   // to remove all
   // notifications.
-  application.applicationIconBadgeNumber = 1;
-  application.applicationIconBadgeNumber = 0;
+  // application.applicationIconBadgeNumber = 1;
+  // application.applicationIconBadgeNumber = 0;
 }
 
 - (BOOL)application:(UIApplication *)application
